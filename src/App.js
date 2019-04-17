c5254078c99a2a8a2ad1e8658be8d725835f4e34
phalbert <ahosea15@gmail.com>
2019-04-17 22:40:29 +0300

    setState: never mutate state directly

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import {Greet} from './components/Greet'
-import Welcome from './components/Welcome'
-import {Hello} from './components/Hello'
-import Message from './components/Message'
+import Counter from './components/Counter'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <img src={logo} className="App-logo" alt="logo" />
-         <Greet name="Bruce" heroName="Superman">
-           <p>This is a children prop</p>
-         </Greet>
-        <Message />
+        <Counter addValue='5'/>
       </div>
     );
   }
